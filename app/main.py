6d667df724fe88d019f2cbaf954140893c7a6deb
Vlad Tymoshko <slradbez@gmail.com>
2024-11-18 15:35:53 +0100

    solution

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ class Animal:
         if self in Animal.alive:
             Animal.alive.remove(self)
 
-    def __repr__(self) -> None:
+    def __repr__(self) -> str:
         return (f"{{Name: {self.name}, "
                 f" Health: {self.health}, "
                 f" Hidden: {self.hidden}}}")
