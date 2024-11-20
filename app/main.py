a7bdaf637a67a02a24647037ce0bcf0f79e8c9ea
Vlad Tymoshko <slradbez@gmail.com>
2024-11-20 14:08:00 +0100

    solution

--- app/main.py
+++ app/main.py
@@ -14,8 +14,8 @@ class Animal:
             Animal.alive.remove(self)
 
     def __repr__(self) -> str:
-        return (f"{{Name: {self.name}, "
-                f" Health: {self.health}, "
+        return (f"{{Name: {self.name},"
+                f" Health: {self.health},"
                 f" Hidden: {self.hidden}}}")
 
 
